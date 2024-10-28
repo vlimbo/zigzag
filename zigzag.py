54e0fbde03cb5436c1c2a62204dd27cd8d90db6e
Vetle Limbodal <vwlimbo@proton.me>
2024-10-28 19:48:16 +0100

    Create function that handles direction toggle
    Being able to change direction is now based on the amount of indents
    added to asterisks.

--- zigzag.py
+++ zigzag.py
@@ -11,8 +11,15 @@ try:
         if indentIncreasing:
             indent += 1
 
-        if indent == 20:
-            indentIncreasing = False
+            if indent == 20:
+                indentIncreasing = False
+
+
+        else:
+            indent -= 1
+
+            if indent == 0:
+                indentIncreasing = True
 
 except KeyboardInterrupt:
     sys.exit()
