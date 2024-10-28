a410e225e6081e51b747a6c3aa839066c5765c52
Vetle Limbodal <vwlimbo@proton.me>
2024-10-28 19:06:37 +0100

    Create function that prints asterisks from right
    Now we have asterisks that prints from right.

--- zigzag.py
+++ zigzag.py
@@ -1,8 +1,17 @@
-def printAsterisks():
+def printAsterisksRight():
     line = '********'
 
     while len(line) < 12:
         line = ' ' + line
         print(line)
 
-printAsterisks();
+def printAsterisksLeft():
+    line = '********'
+    spaces = 3
+    while spaces != 0:
+        print(" " * spaces, end="")
+        print(line)
+        spaces -= 1
+    
+printAsterisksRight()
+printAsterisksLeft()
